8598f28a42c8e6e1ab4a22193bc38d06d1bad121
Nick-2003 <tsunlinicholastam@gmail.com>
2025-04-17 11:13:10 -0700

    roxygen comments work properly, vignette now works

--- R/is_leap.R
+++ R/is_leap.R
@@ -1,19 +1,19 @@
-#’ Is leap year?
-#’ 
-#’ Check if a year is a leap year.
-#’ 
-#’ @param year Year to input
-#’ 
-#’ @return Boolean
-#’ 
-#’ @export
-#’ 
-#’ @examples
-#’ is_leap(2000)
-#’ #> TRUE
-#’ is_leap(1900)
-#’ #> FALSE
-#’ 
+#' Is leap year?
+#' 
+#' Check if a year is a leap year.
+#' 
+#' @param year Year to input
+#' 
+#' @return Boolean
+#' 
+#' @export
+#' 
+#' @examples
+#' is_leap(2000)
+#' #> TRUE
+#' is_leap(1900)
+#' #> FALSE
+#' 
 is_leap <- function(year) {
   if (year <= 0 || year %% 1 != 0) {
     return(stop()) # Throw error if year is 0 or less
